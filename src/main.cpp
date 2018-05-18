eca95196686f7ea8e56bd53b228f74a22badaece
Cathean <ivanshadow19@gmail.com>
2018-05-18 13:27:42 +0700

    Separate other function from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <boost/asio.hpp>
-using namespace boost;
+#include <func_proto.h>
 
 bool is_port_open(unsigned short port_num);
 
@@ -18,18 +17,4 @@ int main()
     }
 
     return 0;
-}
-
-bool is_port_open(unsigned short port_num)
-{
-    using namespace boost::asio;
-    using ip::tcp;
-
-    io_service svc;
-    tcp::acceptor a(svc);
-
-    boost::system::error_code ec;
-    a.open(tcp::v4(), ec) || a.bind({ip::address::from_string("127.0.0.1"), port_num}, ec);
-
-    return ec == error::address_in_use;
 }
